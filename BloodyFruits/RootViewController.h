9914e22441f217d038ab464f2a0f38d2048a4688
Phillipe Casorla Sagot <phillipe@pota-toss.com>
2012-08-11 02:56:56 -0400

    Initial Commit

---
+++ BloodyFruits/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BloodyFruits
+//
+//  Created by Phillipe Casorla Sagot on 8/11/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ BloodyFruits/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BloodyFruits
+//
+//  Created by Phillipe Casorla Sagot on 8/11/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
